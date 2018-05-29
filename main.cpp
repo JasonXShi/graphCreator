52a9047fd148a2cb6f56d8c21ece80297da7f30d
Jason Shi <jasonxshi2002@gmail.com>
2018-05-29 11:47:56 -0700

    input

--- main.cpp
+++ main.cpp
@@ -4,5 +4,32 @@
 using namespace std;
 
 int main(){
+    while(true){
+        cout << endl;
+        cout << "Enter p to print." << endl;
+        cout << "Enter ae to add an edge." << endl;
+        cout << "Enter re to remove an edge." << endl;
+        cout << "Enter av to add a vertex." << endl;
+        cout << "Enter rv to remove a vertex." << endl;
+        cout << "Enter f to find." << endl;
+        cout << "> ";
+        char* input = new char[100];
+        cin.getline(input, 100);
+        if(strcmp(input, "p")==0){
+         
+        }else if(strcmp(input, "ae")==0){
 
+        }else if(strcmp(input, "re")==0){
+        
+        }else if(strcmp(input, "av")==0){
+        
+        }else if(strcmp(input, "rv")==0){
+        
+        }else if(strcmp(input, "f")==0){
+        
+        }else{
+            cout << endl;
+            cout<< "Not a valid input."<< endl;
+        }
+    }
 }
